607f73b3887e9a17bf78bc02e964a022a7181469
mohitsharma44 <mohitsharma44@gmail.com>
2017-04-25 18:06:09 -0400

    script to start the jupyter notebook

---
+++ image/start-notebook.sh
@@ -0,0 +1,4 @@
+#   --ip 0.0.0.0: Allow all IP access.
+#   --no-browser: Don't open browser from command line.
+#   --notebook-dir: particular directory where notebooks are.
+jupyter-notebook --ip 0.0.0.0 --no-browser --notebook-dir=/home
